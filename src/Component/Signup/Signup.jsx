3124f80bb866d90d2748aee898267796f73dbbc2
Source-Code-007 <utsho926@gmail.com>
2023-04-14 14:23:43 +0600

    login form added

--- src/Component/Signup/Signup.jsx
+++ src/Component/Signup/Signup.jsx
@@ -26,13 +26,19 @@ const Signup = () => {
     }
 
     return (
-        <div className='min-h-screen bg-slate-100 flex flex-col justify-evenly items-center'>
-            <div>
+        <div className='min-h-screen bg-slate-200 flex flex-col justify-evenly items-center'>
+            <div className='space-y-4 font-bold text-xl text-slate-50 px-10 py-5 bg-slate-50 rounded-lg shadow-inner'>
+                <h2 className='font-bold text-slate-800 text-center text-3xl'>Login here</h2>
+                <form action="#" className='flex flex-col gap-5'>
+                    <input type="email" name="" id="" className='p-4 text-slate-700 rounded-lg bg-slate-200 shadow-inner border-none' placeholder='Your email here' />
+                    <input type="password" name="" id="" className='p-4 text-slate-700 rounded-lg bg-slate-200 shadow-inner border-none' placeholder='Your email here'/>
+                    <input type="submit" value="submit" className='p-4 rounded-lg bg-emerald-500 shadow' />
+                </form>
                 {
                     !user ?
-                        <div className='space-x-5'> <button onClick={signInWithGoogle} className='px-5 py-3 bg-green-500 rounded-lg shadow-lg font-bold text-slate-50'>Sign in with Google</button> 
-                        <button className='px-5 py-3 bg-green-500 rounded-lg shadow-lg font-bold text-slate-50'>Sign in with Github</button> </div> :
-                        <button onClick={signOutFunc} className='px-5 py-3 bg-green-500 rounded-lg shadow-lg font-bold text-slate-50'>Sign Out</button>
+                        <div className='space-x-5'> <button onClick={signInWithGoogle} className='px-5 py-3 bg-green-500 rounded-lg shadow'>Sign in with Google</button> 
+                        <button className='px-5 py-3 bg-green-500 rounded-lg shadow'>Sign in with Github</button> </div> :
+                        <button onClick={signOutFunc} className='px-5 py-3 bg-green-500 rounded-lg shadow'>Sign Out</button>
                 }
             </div>
 
