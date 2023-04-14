428754fb72008abcd564985dc533e280f7454708
Source-Code-007 <utsho926@gmail.com>
2023-04-14 11:15:13 +0600

    firebase install and config file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// ------ **** ------ //
+// ------ **** ------ //
+// ------ **** ------ //
+// [Danger to share firebase config]
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPmJKtZURvCkRJhZb1VhG2dgTxK-_xt1U",
+  authDomain: "educative-9796f.firebaseapp.com",
+  projectId: "educative-9796f",
+  storageBucket: "educative-9796f.appspot.com",
+  messagingSenderId: "57307661679",
+  appId: "1:57307661679:web:33e225caabbfc569bfd49a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
